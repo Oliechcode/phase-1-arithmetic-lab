c3a6c23c99024e1fa2f416f38d6242f0d7987057
Linet Oliech <oliechlin@gmail.com>
2022-08-18 18:12:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+let random = Math.floor (Math.random()>0) 
+let num3 = 34
+let num4 = 5
+let mod = num3 % num4
+const max = Math.max (4,7,8,10,15,20)
+
+
+
